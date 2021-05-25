6c3b0b46d063a9de8c68d51ec1a784d4db8cb134
katoyip3 <77433508+angusovo@users.noreply.github.com>
2021-05-25 18:00:22 +0800

    private routing + bug fixed
    1.changed firebase rules
    2. added private routing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './Pages/SignUp';
 import { AuthProvider } from './Context/AuthContext';
 import Login from './Pages/Login';
 import SellCarInfo from './Pages/SellCarInfo';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
@@ -26,8 +27,12 @@ function App() {
             <Route path="/blogs/:blogId" component={Blog}/>
             <Route path="/about" component={About}/>
             <Route path="/cars/:carId" component={Cars}/>
-            <Route path="/signup" component={SignUp}/>
-            <Route path="/login" component={Login}/>
+            <PrivateRoute exact path="/signup">
+              <SignUp/>
+            </PrivateRoute>
+            <PrivateRoute exact path="/login">
+              <Login />
+            </PrivateRoute>
             <Route path="/sellcarinfo" component={SellCarInfo}/>
           </Switch>
           <Footer/>
---
+++ src/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Redirect } from "react-router-dom"
+import {useAuth} from "./Context/AuthContext"
+
+const PrivateRoute=({children, ...rest})=>{
+    const { currentUser } = useAuth()
+
+    return (
+        <Route {...rest} render={()=> !currentUser? (children):(<Redirect to={"/"}/>)}/>
+    )
+}
+
+export default PrivateRoute
